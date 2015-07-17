3cf105dc2644f923025db27db71b013a30d78e19
Jonathan Daniels <jondaniels.is@gmail.com>
2015-07-17 17:32:46 +0000

    Added profile's new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,7 @@
 class ProfilesController < ApplicationController
-    
+    def new
+        # form where a user can fill out thier own profile.
+        @user = User.find( params[:user_id] )
+        @profile = @user.build_profile # we can do this because of the nested route in routes.rb
+    end
 end
