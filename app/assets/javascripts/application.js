0205ca81f0ab3f2245fc10614f7acfc0abc080f6
Jonathan Daniels <jondaniels.is@gmail.com>
2015-07-20 16:08:26 +0000

    Added form partial and edit action

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,66 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require_tree .
+
+
+
+
+$(document).ready(function(){
+    $('.fade').hide();
+    
+    var i = function(){
+        $('.alert').before("<i class= 'fa fa-check-circle fa-2x alert-close'><i>")
+        $('.alert-close').hide();
+    };
+    
+    var AlertIn = function(){
+        $('.fade').hide();
+        $('.alert').hide();
+        $('.alert').addClass(' text-center large-text');
+        $('.fade').fadeIn(150, function(){
+            $('.alert').fadeIn(250);
+            $('.alert-close').fadeIn(250);
+            });
+    };
+    
+    
+    
+    var hover = function(){
+         $('.alert-container').hover(function(){ 
+             $('.alert-close').fadeTo(450, 0.5); 
+             //$('.alert').fadeTo(200,0.5); 
+             
+         },function(){ 
+             $('.alert-close').fadeTo(650, 1);
+             //$('.alert').fadeTo(50,1);
+         });
+        
+    };
+        
+        var AlertOut = function() {
+        $('.alert').fadeOut(350);
+        $('.fade').fadeOut(450);
+        $('.alert-close').remove();
+        $('.alert').remove();
+    };
+    
+    var click = function(){
+        $('.alert-container').on('click', AlertOut);
+    };
+    
+    
+    if ($('.alert')[0]){
+        i();
+        AlertIn();
+        hover();
+        click();
+    }
+    else{
+        $('.fade').hide();
+        $('.alert').removeClass(' text-center large-text')
+    }
+    
+    
+});
+
+
